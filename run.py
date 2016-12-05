9dd10b2d2a22c13f8f5a79b725dec4240f4893eb
Marc <strategy155@gmail.com>
2016-12-05 07:01:53 +0300

    finished sets

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import bs4, requests
+import bs4, requests, re
 
 WEB = 'https://news.yandex.ru/yandsearch?lr=213&cl4url=ria.ru%2Feconomy%2F20161205%2F1482815380.html&lang=ru&rubric=computers&from=index'
 text = requests.get(WEB).text
@@ -9,7 +9,24 @@ webs_ls = []
 for tag in tags:
     if tag['href'].startswith('http'):
         webs_ls.append(tag['href'])
-text = requests.get(webs_ls[0]).text
-soup = bs4.BeautifulSoup(text, 'html.parser')
-for elem in soup.find_all('p'):
-    print(elem.text)
+word_sets = []
+for elem in webs_ls:
+    text = requests.get(elem).text
+    soup = bs4.BeautifulSoup(text, 'html.parser')
+
+    for elem in soup.find_all('p'):
+        if len(elem.parent.parent.find_all('p'))>1:
+            right_tags = elem.parent.parent.find_all('p')
+            break
+    new_words = []
+    for elem in right_tags:
+        wut=re.sub('[^а-яёa-z0-9]'," ", elem.text.lower()).split(' ')
+        new_words+=(filter(None, wut))
+    word_sets.append(set(new_words))
+uniqueness = word_sets[0]
+lulness = word_sets[0]
+for setto in word_sets:
+    uniqueness = uniqueness & setto
+    lulness = lulness ^ setto
+print(uniqueness)
+print(lulness)
